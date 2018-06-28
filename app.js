7061fa198820ecdb677aa983afbf6527e292d3bd
thepunkprogrammer <rkodekar@zoho.com>
2018-06-29 01:46:05 +0530

    What makes a http request

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=yakub%20street%20Ibrahim%20rehmatulla%20road%20mandvi%20mumbai%20400003',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    var result = body.results[0];
+    console.log(`Address: ${result.formatted_address}`);
+    console.log(`Latitude: ${result.geometry.location.lat}`);
+    console.log(`Longitude: ${result.geometry.location.lng}`);
 })
