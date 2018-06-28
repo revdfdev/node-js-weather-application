6e5c56042c62ed4cb4e46ed58873d3e5f222a386
thepunkprogrammer <rkodekar@zoho.com>
2018-06-29 01:29:09 +0530

    Pretty printing objects

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=yakub%20street%20Ibrahim%20rehmatulla%20road%20mandvi%20mumbai%20400003',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 })
