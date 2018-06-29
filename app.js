06765cd0abb2314b364f046d09b28e4d32e27323
thepunkprogrammer <rkodekar@zoho.com>
2018-06-30 01:57:37 +0530

    Add abstract callbacks

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const request = require('request');
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+const request = require("request");
+const yargs = require("yargs");
+const geocode = require("./geocode/geocode");
 
-const argv = yargs.options({
+const argv = yargs
+  .options({
     a: {
-        demand: true,
-        alias: 'address',
-        describe: 'Address to fetch the weather for',
-        string: true
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch the weather for",
+      string: true
     }
-}).help()
-    .alias('help', 'h')
-    .argv;
+  })
+  .help()
+  .alias("help", "h").argv;
 
-
-geocode.geoCodeAddress(argv.address, (error, data) => {
-    if (error !== undefined) {
-        console.log(error);
-    } else {
-        var result = data.results[0];
-        console.log(`Address: ${result.formatted_address}`);
-        console.log(`Latitude: ${result.geometry.location.lat}`);
-        console.log(`Longitude: ${result.geometry.location.lng}`); 
-    }
+geocode.geoCodeAddress(argv.address, (error, result) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Address: ${result.address}`);
+    console.log(`Latitude: ${result.latitude}`);
+    console.log(`Longitude: ${result.longitude}`);
+  }
 });
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -7,11 +7,15 @@ geoCodeAddress = (address, callback) => {
         json: true
     }, (error, response, body) => {
         if (error) {
-            callback("Unable to connect to google servers", undefined);
+            callback("Unable to connect to google servers");
         } else if (body.status === "ZERO_RESULTS") {
-            callback('Unable to find that address', undefined);
+            callback('Unable to find that address');
         } else if (body.status === "OK") {
-            callback(undefined, body);
+            callback(undefined, {
+              address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
         }
     })
 };
